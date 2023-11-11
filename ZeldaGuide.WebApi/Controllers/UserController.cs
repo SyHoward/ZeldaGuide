02918c1f47c0686410647fb21909fdac3a505558
Austin Carie <austincarie0209@gmail.com>
2023-11-11 14:34:23 -0500

    POST User endpoint created

--- ZeldaGuide.WebApi/Controllers/UserController.cs
+++ ZeldaGuide.WebApi/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using ZeldaGuide.Models.Responses;
+using ZeldaGuide.Models.User;
 using ZeldaGuide.Services.User;
 
 namespace ZeldaGuide.WebApi.Controllers;
@@ -11,5 +13,23 @@ public class UserController : ControllerBase
     public UserController(IUserService userService)
     {
         _userService = userService;
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var registerResult = await _userService.RegisterUserAsync(model);
+        if (registerResult)
+        {
+            TextResponse response = new("User was registered.");
+            return Ok(response);
+        }
+        
+        return BadRequest(new TextResponse("User could not be registered."));
     }
 }
