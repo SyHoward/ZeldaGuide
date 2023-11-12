10fa0f6c3719ad70fb071eb646e23229313fff02
Sy Howard <symanthacrh@gmail.com>
2023-11-12 11:37:49 -0500

    ToDoFK1

---
+++ ZeldaGuide.Data/Migrations/20231112163621_AddToDoFK.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZeldaGuide.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddToDoFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "ToDos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_OwnerId",
+                table: "ToDos",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Users_OwnerId",
+                table: "ToDos",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Users_OwnerId",
+                table: "ToDos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ToDos_OwnerId",
+                table: "ToDos");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "ToDos");
+        }
+    }
+}
