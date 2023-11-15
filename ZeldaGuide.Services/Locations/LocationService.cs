96522acfbd5642acc0c247d2f2419863afc56f80
Ben Strycker <buzzcutben@hotmail.com>
2023-11-15 18:30:36 -0500

    Commiting.

--- ZeldaGuide.Services/Locations/LocationService.cs
+++ ZeldaGuide.Services/Locations/LocationService.cs
@@ -1,8 +1,27 @@
-using Location.Services.Location;
-
 namespace ZeldaGuide.Services.Location;
 
 public class LocationService : ILocationService
 {
+     private readonly ApplicationDbContext _dbContext;
+    private readonly int _Location;
     
-}
+    public LocationService(UserManager<UserEntity> userManager,
+                         dbContext)
+    private readonly ApplicationDbContext _dbContext;
+    private readonly int _userId;
+    
+    public LocationService(UserManager<UserEntity> userManager,
+                        SignInManager<UserEntity>signInManager,
+                        ApplicationDbContext dbContext)
+    
+    {
+        var currentUser = signInManager.Context.User;
+        var userIdClaim = userManager.GetUserId(currentUser);
+        var hasValidId = int.TryParse(userIdClaim, out _userId);
+
+        if (hasValidId == false)
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<ToDoListItem>> GetAllToDoAsync();
+}    
